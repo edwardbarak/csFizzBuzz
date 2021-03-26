c3815b4b76d3f75d08f9abde3d0f613f0a21d0c9
Edward Barak <edward.barak@gmail.com>
2021-03-26 13:31:23 -0400

    fizzbuzz

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -4,10 +4,28 @@ namespace myApp
 {
     class Program
     {
+        static string fizzbuzz(int number){
+            string output;
+            if (number % 15 == 0) {
+                output = "fizzbuzz";
+            } else if (number % 3 == 0) {                
+                output = "fizz";
+            } else if (number % 5 == 0) {
+                output = "buzz";
+            } else {
+                output = number.ToString();
+            }
+
+            return output;
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("The current time is: " + DateTime.Now);
+            // Console.WriteLine("Hello World!");
+            // Console.WriteLine("The current time is: " + DateTime.Now);
+            for (int i = 1; i <= 20; i++){
+                Console.WriteLine(fizzbuzz(i));
+            }            
         }
     }
 }
